07ee342ea13715263db1d545f001cdad25a42808
AlexGNE <an2013@rit.edu>
2020-09-18 17:52:28 -0400

    9/18 - second update
    1. Changed notation of comments and code indexing
    2. Added interpretation of ANOVA results, partial interpretation of Bonferroni results
    3. Still missing: complete interpretation of Bonferroni, interpretation of Benjamini-Hochberg, and rest of the problems.

--- ElenesAGN_macOS_A1.R
+++ ElenesAGN_macOS_A1.R
@@ -1,16 +1,7 @@
 # Alejandro G.N. Elenes
-#### 1 ####
+# Problem 1 ----
 # Code written in macOS Catalina version 10.15.6
 # R packages needed to run this script:
-# install.packages("ggplot2")
-# install.packages("psych")
-# install.packages("readxl")
-# install.packages("openxlsx")
-# install.packages("reshape2")
-# install.packages("dplyr")
-# install.packages("ggpubr")
-# install.packages("rstatix")
-# install.packages("Rmisc")
 library(ggplot2)
 library(psych)
 library(readxl)
@@ -23,7 +14,7 @@ library(rstatix)
 library(Rmisc)
 # Data files necessary to run this script:
 # 3_1_Generator_Y2018.xlsx
-#### 2 ####
+# Problem 2 ----
 # Create a dataset of 5000 random numbers generated in R 
 # using any model distribution you like. Write an R script 
 # that reports the sample mean and sample standard deviation 
@@ -37,7 +28,7 @@ hist(random_data, prob = TRUE) # plot a histogram of random data
 lines(density(random_data,na.rm=T),col="red",lwd=2) # over histogram, add density line
 curve(dnorm(x, mean=mean(random_data), sd=sd(random_data)), add=TRUE) # over histogram, plot normal distribution based on random data
 
-#### 3 ####
+# Problem 3 ----
 # To automate the printing of stat test output in R you will 
 # want to explore the ‘sink’ and ‘unsink’ functions in R. 
 # Use your script to create a file called ‘desc.txt’ and 
@@ -53,7 +44,7 @@ lines(density(random_data,na.rm=T),col="red",lwd=2)
 curve(dnorm(x, mean=mean(random_data), sd=sd(random_data)), add=TRUE)
 dev.off() # stop printing to pdf
 
-#### 4 ####
+# Problem 4 ----
 
 # Find an appropriate data set for a one-way ANOVA. Shape 
 # the data so you can use the read.table function from an 
@@ -75,7 +66,7 @@ dev.off() # stop printing to pdf
 # Be sure to include verbal interpretation printed to screen 
 # and/or output files.
 
-#### 4.1 - about the dataset ####
+  # 4.1 - about the dataset ----
 
 # I will look for trends in power plants retirements in the US. e.g:
 # how old are power plants at age of retirement?
@@ -91,8 +82,8 @@ dev.off() # stop printing to pdf
 # files. I use the sheet 'Retired and Canceled' from 
 # the file '3_1_Generator_Y2018.xlsx'
 
-#### 4.2 - code ####
-#### 4.2.1 - load data, create variables ####
+  # 4.2 - code ----
+  # 4.2.1 - load data, create variables ----
 Retirement_dataset <- read.xlsx("3_1_Generator_Y2018.xlsx", sheet = "Retired and Canceled", startRow = 2, na.strings = " ")
 Retirement_dataset <- Retirement_dataset[complete.cases(Retirement_dataset$Retirement.Year),] # drop missing values (cancelled power plants)
 Online <- Retirement_dataset$Operating.Year # Year plant went online / was born
@@ -101,7 +92,7 @@ Source <- Retirement_dataset$Energy.Source.1 # Primary fuel
 Capacity <- Retirement_dataset$`Nameplate.Capacity.(MW)` # Plant capacity in MW
 Age <- Retired - Online # Plant phyisical life / age
 Fuel_unique_categories <- unique(Source)
-#### 4.2.2 - Group into higher level categories based on fuel type for easier tractability####
+  # 4.2.2 - Group into higher level categories based on fuel type for easier tractability----
 Fuel_categories <- matrix(0,length(Retired),1)
 Fuel_categories[which(Source == "ANT" )] <- "COAL"
 Fuel_categories[which(Source == "BIT" )] <- "COAL"
@@ -142,10 +133,10 @@ Fuel_categories[which(Source == "TDF" )] <- "OTHER"
 Fuel_categories[which(Source == "MWH" )] <- "OTHER"
 Fuel_categories[which(Source == "OTH" )] <- "OTHER"
 # Went from 31 to 10 categories
-#### 4.2.3 - One way ANOVA ####
+  # 4.2.3 - One way ANOVA ----
 age.anova <- oneway.test(Age~Fuel_categories)
 age.anova
-#### 4.2.4 - Error bar chart ####
+  # 4.2.4 - Error bar chart ----
 
 # Try using geom_errorbar
 std.err.plot.agevsfuelcategories <- ggplot(Retirement_dataset, aes(x = Fuel_categories, y = Age)) +
@@ -170,33 +161,28 @@ ErrorBar_AgeVSFuelCat <- ggplot(stats, aes(x=Fuel_categories, y=Age)) +
   theme(axis.text.x=element_text(angle=15))
 ErrorBar_AgeVSFuelCat # print plot
 
-#### 4.2.5 - Pairwise t-tests ####
-
+  # 4.2.5 - Pairwise t-tests ----
 # Bonferroni
 age.pairwise.bonf <- pairwise.t.test(Age, Fuel_categories, p.adj = "bonf")
 age.pairwise.bonf
-
 # Benjamini-Hochberg
 age.pairwise.benjhoch <- pairwise.t.test(Age, Fuel_categories, p.adj = "BH")
 age.pairwise.benjhoch
-
 # Some descriptive statistics
-box.plot.agevsfuelcategories <- ggplot(Retirement_dataset, mapping = aes(x = Fuel_categories, y = Age)) + 
-  geom_boxplot() + 
-  labs(x = 'Fuel categories', y = 'Phyiscal life [years]') + 
-  theme(axis.text.x=element_text(angle=15), 
-        panel.background = element_rect(fill = 'grey50'))
-box.plot.agevsfuelcategories # print plot
-
-#### 4.2.6 - Export & interpretation ####
-
+box.plot.agevsfuelcategories <- ggplot(Retirement_dataset, mapping = aes(x = Fuel_categories, y = Age)) + geom_boxplot() + labs(x = 'Fuel categories', y = 'Phyiscal life [years]') + theme(axis.text.x=element_text(angle=15), panel.background = element_rect(fill = 'grey50'))
+box.plot.agevsfuelcategories
+  # 4.2.6 - Export & interpretation ----
 sink(file = 'A1Problem4_Text.txt') # used new text file to avoid rewriting of previous exercises
 age.anova
+writeLines("The one-way analysis of variance (ANOVA) compares the means between three or more independent (unrelated) groups. Here, the groups are defined by the technology categories (biomass plants make up group 1, coal plants group 2, etc...) and the variable whose mean we are comparing is the power plant age or lifespan.   \n \nThe null hypothesis assumes the mean power plant lifespan between groups is statistically the same i.e. that any differences between them are random, while the alternative hypothesis states that the differences between at least 2 of the groups compared are not random i.e. power plants lifespan is consistently different at least between 2 of the groups.   \n\nSince the p-value is less than 0.05, we must reject the null hypothesis, which indicates that power plants lifespan is consistently different at least between two of the technologies analyzed. \n")
+writeLines("In the previous one-way ANOVA test we rejected the null hypothesis, implying that there are significant differences between at least two technologies. But this does not tell us which technologies are significantly different; for this purpose, we can use the Bonferroni method.   \n\nBonferroni's method helps to answer 'which technologies are significantly different from each other?' by performing a pairwise comparison of the means i.e. by comparing all the means against each other e.g. given 3 technologies: coal, biomass & gas, compare all pairs of them: coal vs biomass, coal vs gas, and biomass vs gas. For our case, the number of pairs is given by k = (a)*(a-1)*(1/2) where a = number of groups. Since a = 10, then here we will compare k = (10)*(10-1)*(1/2) = (5)*(9) = 45 pairs, as shown below.   \n\n")
 age.pairwise.bonf
+writeLines("Many of the comparisons yielded significant differences. Nuclear plants have the least significant differences in average lifespan with respect to the rest of technologies and it only differs significantly from that of hydroelectric plants, which on the other hand are significantly different from all the other groups, followed by coal & gas with 8, oil & other with 5, and biomass, wind, solar & geothermal with 4.")
 age.pairwise.benjhoch
+writeLines("")
 sink()
-
 pdf(file = 'A1Problem4_Plots.pdf') # trigger printing to pdf file
 ErrorBar_AgeVSFuelCat
+print("")
 dev.off() # stop printing to pdf
 
